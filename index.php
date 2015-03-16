b470e0eac2024df59109ded951e067ebbb7980a5
Prawee Wongsa <prawee@hotmail.com>
2015-03-16 17:59:55 +0700

    testing show index

--- index.php
+++ index.php
@@ -4,4 +4,26 @@
  * @author Prawee Wongsa <konkeanweb@gmail.com>
  */
 require 'vendor/autoload.php';
+require 'systems/debug.php';
+
+$config=[
+    'debug'=>true,
+    'templates.path'=>'./templates'
+];
+
+$app = new \Slim\Slim($config);
+
+//Debug::printr($app);
+
+$app->get('/', function () {
+    echo "Hello World";
+});
+
+//$app->config('debug',false);
+//Debug::printr($app->container->settings);
+//echo $app->config('templates.path');
+$app->run();
+
+
+
 
