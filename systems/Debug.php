dfc10f9514556d92735c22795844326a1e2312bd
Prawee Wongsa <prawee@hotmail.com>
2015-03-16 17:55:11 +0700

    add Debug class for print data

---
+++ systems/Debug.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * 2015-03-16
+ * @author Prawee Wongsa <konkeanweb@gmail.com>
+ */
+
+class debug{
+    public static function printr($data){
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+    }
+}
